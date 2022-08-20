e850800379cf21221a63efe8e95a3f3a5d0aff3d
Softypo <ggrsoft@gmail.com>
2022-08-19 23:58:27 -0700

    first commit

---
+++ modelviewer/_imports_.py
@@ -0,0 +1,5 @@
+from .modelviewer import modelviewer
+
+__all__ = [
+    "modelviewer"
+]
---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import modelviewer from './components/modelviewer.react';
+
+export {
+    modelviewer
+};
